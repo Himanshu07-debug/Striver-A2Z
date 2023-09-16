b5059b4258e83e96829f2e6810d2b981982c1ba9
T_Himanshu_123 <himanshusharma2002.2000@gmail.com>
2023-09-16 23:02:31 +0530

    Update Next-Permutation.cpp

--- 2.Array/Medium/Next-Permutation.cpp
+++ 2.Array/Medium/Next-Permutation.cpp
@@ -1,3 +1,11 @@
 //  Rearrange the numbers of the given array into the lexicographically next greater permutation of numbers.
 // If such an arrangement is not possible, it must rearrange to the lowest possible order 
 // (i.e., sorted in ascending order).
+
+
+// NAIVE -> Generate all Permutations + Linear search for the I/P permutation + return the next Permutation
+//          Time : O (N! * N )
+
+// BETTER -> using STL built-in function  -> O(N)
+
+//  OPTIMAL ->
