33f0068ad7e454dd8b250bafd2a22360aa6703f5
T_Himanshu_123 <himanshusharma2002.2000@gmail.com>
2023-09-23 23:55:33 +0530

    updated

--- 11.Graph/Start/DFS.cpp
+++ 11.Graph/Start/DFS.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 class Solution {
   private: 
+
     void dfs(int node, vector<int> adj[], int vis[], vector<int> &ls) {
         vis[node] = 1; 
         ls.push_back(node); 
@@ -14,24 +15,28 @@ class Solution {
             }
         }
     }
+
   public:
-    // Function to return a list containing the DFS traversal of the graph.
-    vector<int> dfsOfGraph(int V, vector<int> adj[]) {
-        int vis[V] = {0}; 
-        int start = 0;
-        // create a list to store dfs
-        vector<int> ls; 
-        // call dfs for starting node
-        dfs(start, adj, vis, ls); 
-        return ls; 
-    }
+
+        // Function to return a list containing the DFS traversal of the graph.
+        vector<int> dfsOfGraph(int V, vector<int> adj[]) {
+            int vis[V] = {0}; 
+            int start = 0;
+            // create a list to store dfs
+            vector<int> ls; 
+            // call dfs for starting node
+            dfs(start, adj, vis, ls); 
+            return ls; 
+        }
 };
 
+// Adding an Edge
 void addEdge(vector <int> adj[], int u, int v) {
     adj[u].push_back(v);
     adj[v].push_back(u);
 }
 
+// PRINTING
 void printAns(vector <int> &ans) {
     for (int i = 0; i < ans.size(); i++) {
         cout << ans[i] << " ";
