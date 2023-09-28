33c89bcac3e87444ee31ec2d2a72558319eb5f25
T_Himanshu_123 <himanshusharma2002.2000@gmail.com>
2023-09-28 10:25:57 +0530

    Graph Updated

--- 11.Graph/Start/DFS.cpp
+++ 11.Graph/Start/DFS.cpp
@@ -7,6 +7,7 @@ class Solution {
     void dfs(int node, vector<int> adj[], int vis[], vector<int> &ls) {
         vis[node] = 1; 
         ls.push_back(node); 
+
         // traverse all its neighbours
         for(auto it : adj[node]) {
             // if the neighbour is not visited
@@ -22,10 +23,13 @@ class Solution {
         vector<int> dfsOfGraph(int V, vector<int> adj[]) {
             int vis[V] = {0}; 
             int start = 0;
+
             // create a list to store dfs
             vector<int> ls; 
+
             // call dfs for starting node
             dfs(start, adj, vis, ls); 
+            
             return ls; 
         }
 };
